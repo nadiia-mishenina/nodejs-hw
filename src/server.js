af6b1c3f82764923615cdaf3437928e5efd83e76
nadiia-mishenina <ya.nadegda2014@gmail.com>
2026-03-21 22:16:04 +0200

    Switch to ES modules and add eslint config

--- src/server.js
+++ src/server.js
@@ -1,19 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const pinoHttp = require('pino-http');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pinoHttp from 'pino-http';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-
+// middleware
 app.use(cors());
 app.use(express.json());
-app.use(pinoHttp());
+app.use(
+  pinoHttp({
+    transport:
+      process.env.NODE_ENV !== 'production'
+        ? {
+            target: 'pino-pretty',
+          }
+        : undefined,
+  }),
+);
 
-
+// routes
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
 });
@@ -27,15 +36,14 @@ app.get('/test-error', () => {
   throw new Error('Simulated server error');
 });
 
-
+// 404 middleware
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-
+// 500 middleware
 app.use((err, req, res, next) => {
- 
-  if (req.log) req.log.error(err);
+  req.log?.error(err);
   res.status(500).json({ message: err.message });
 });
 
