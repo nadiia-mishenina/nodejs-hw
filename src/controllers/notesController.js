468c66941d2fda17e79e9eb628ee00f9b2eea6c9
nadiia-mishenina <ya.nadegda2014@gmail.com>
2026-03-22 00:45:39 +0200

    Add auth, sessions and private notes

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -3,11 +3,12 @@ import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search = '' } = req.query;
-
   const currentPage = Number(page);
   const currentPerPage = Number(perPage);
 
-  const filter = {};
+  const filter = {
+    userId: req.user._id,
+  };
 
   if (tag) {
     filter.tag = tag;
@@ -38,7 +39,10 @@ export const getAllNotes = async (req, res) => {
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
 
-  const note = await Note.findById(noteId);
+  const note = await Note.findOne({
+    _id: noteId,
+    userId: req.user._id,
+  });
 
   if (!note) {
     throw createHttpError(404, 'Note not found');
@@ -48,14 +52,21 @@ export const getNoteById = async (req, res) => {
 };
 
 export const createNote = async (req, res) => {
-  const note = await Note.create(req.body);
+  const note = await Note.create({
+    ...req.body,
+    userId: req.user._id,
+  });
+
   res.status(201).json(note);
 };
 
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
 
-  const note = await Note.findByIdAndDelete(noteId);
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+    userId: req.user._id,
+  });
 
   if (!note) {
     throw createHttpError(404, 'Note not found');
@@ -67,10 +78,17 @@ export const deleteNote = async (req, res) => {
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
 
-  const note = await Note.findByIdAndUpdate(noteId, req.body, {
-    returnDocument: 'after',
-    runValidators: true,
-  });
+  const note = await Note.findOneAndUpdate(
+    {
+      _id: noteId,
+      userId: req.user._id,
+    },
+    req.body,
+    {
+      returnDocument: 'after',
+      runValidators: true,
+    },
+  );
 
   if (!note) {
     throw createHttpError(404, 'Note not found');
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
@@ -8,6 +9,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRouter from './routes/notesRoutes.js';
+import authRouter from './routes/authRoutes.js';
 
 dotenv.config();
 
@@ -20,8 +22,13 @@ const bootstrap = async () => {
 
   app.use(logger);
   app.use(express.json());
-  app.use(cors());
+  app.use(cors({
+    credentials: true,
+    origin: true,
+  }));
+  app.use(cookieParser());
 
+  app.use(authRouter);
   app.use(notesRouter);
 
   app.use(notFoundHandler);
